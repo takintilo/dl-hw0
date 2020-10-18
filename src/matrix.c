3ff07dd15c77e2f4006143d471c8afedc5c20e0e
Timothy Akintilo <timtobi1@gmail.com>
2020-10-17 21:15:02 -0700

    through 1.3

--- src/matrix.c
+++ src/matrix.c
@@ -1,10 +1,9 @@
 #include "matrix.h"
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <math.h>
-
 
 // Make empty matrix filled with zeros
 // int rows: number of rows in matrix
@@ -16,7 +15,7 @@ matrix make_matrix(int rows, int cols)
     m.rows = rows;
     m.cols = cols;
     m.shallow = 0;
-    m.data = calloc(m.rows*m.cols, sizeof(float));
+    m.data = calloc(m.rows * m.cols, sizeof(float));
     return m;
 }
 
@@ -28,9 +27,9 @@ matrix random_matrix(int rows, int cols, float s)
 {
     matrix m = make_matrix(rows, cols);
     int i, j;
-    for(i = 0; i < rows; ++i){
-        for(j = 0; j < cols; ++j){
-            m.data[i*cols + j] = 2*s*(rand()%1000/1000.0) - s;    
+    for (i = 0; i < rows; ++i) {
+        for (j = 0; j < cols; ++j) {
+            m.data[i * cols + j] = 2 * s * (rand() % 1000 / 1000.0) - s;
         }
     }
     return m;
@@ -51,8 +50,12 @@ void free_matrix(matrix m)
 matrix copy_matrix(matrix m)
 {
     matrix c = make_matrix(m.rows, m.cols);
-    // TODO: 1.1 - Fill in the new matrix
-
+    int i, j;
+    for (i = 0; i < m.rows; ++i) {
+        for (j = 0; j < m.cols; ++j) {
+            c.data[i * m.cols + j] = m.data[i * m.cols + j];
+        }
+    }
 
     return c;
 }
@@ -62,9 +65,13 @@ matrix copy_matrix(matrix m)
 // returns: matrix, result of transposition
 matrix transpose_matrix(matrix m)
 {
-    // TODO: 1.2 - Make a matrix the correct size, fill it in
-    matrix t = make_matrix(1,1);
-
+    matrix t = make_matrix(m.cols, m.rows);
+    int i, j;
+    for (i = 0; i < t.rows; ++i) {
+        for (j = 0; j < t.cols; ++j) {
+            t.data[i * t.cols + j] = m.data[j * t.rows + i];
+        }
+    }
 
     return t;
 }
@@ -78,6 +85,15 @@ void axpy_matrix(float a, matrix x, matrix y)
     assert(x.cols == y.cols);
     assert(x.rows == y.rows);
     // TODO: 1.3 - Perform the weighted sum, store result back in y
+    int i, j, position;
+    float result;
+    for (i = 0; i < x.rows; ++i) {
+        for (j = 0; j < x.cols; ++j) {
+            position = i * x.cols + j;
+            result = a * x.data[position] + y.data[position];
+            y.data[position] = result;
+        }
+    }
 }
 
 // Perform matrix multiplication a*b, return result
@@ -88,8 +104,6 @@ matrix matmul(matrix a, matrix b)
     matrix c = make_matrix(a.rows, b.cols);
     // TODO: 1.4 - Implement matrix multiplication. Make sure it's fast!
 
-
-
     return c;
 }
 
@@ -99,9 +113,9 @@ matrix matmul(matrix a, matrix b)
 void scal_matrix(float s, matrix m)
 {
     int i, j;
-    for(i = 0; i < m.rows; ++i){
-        for(j =0 ; j < m.cols; ++j){
-            m.data[i*m.cols + j] *= s;
+    for (i = 0; i < m.rows; ++i) {
+        for (j = 0; j < m.cols; ++j) {
+            m.data[i * m.cols + j] *= s;
         }
     }
 }
@@ -111,37 +125,40 @@ void print_matrix(matrix m)
 {
     int i, j;
     printf(" __");
-    for(j = 0; j < 16*m.cols-1; ++j) printf(" ");
+    for (j = 0; j < 16 * m.cols - 1; ++j)
+        printf(" ");
     printf("__ \n");
 
     printf("|  ");
-    for(j = 0; j < 16*m.cols-1; ++j) printf(" ");
+    for (j = 0; j < 16 * m.cols - 1; ++j)
+        printf(" ");
     printf("  |\n");
 
-    for(i = 0; i < m.rows; ++i){
+    for (i = 0; i < m.rows; ++i) {
         printf("|  ");
-        for(j = 0; j < m.cols; ++j){
-            printf("%15.7f ", m.data[i*m.cols + j]);
+        for (j = 0; j < m.cols; ++j) {
+            printf("%15.7f ", m.data[i * m.cols + j]);
         }
         printf(" |\n");
     }
     printf("|__");
-    for(j = 0; j < 16*m.cols-1; ++j) printf(" ");
+    for (j = 0; j < 16 * m.cols - 1; ++j)
+        printf(" ");
     printf("__|\n");
 }
 
 // Used for matrix inversion
 matrix augment_matrix(matrix m)
 {
-    int i,j;
-    matrix c = make_matrix(m.rows, m.cols*2);
-    for(i = 0; i < m.rows; ++i){
-        for(j = 0; j < m.cols; ++j){
-            c.data[i*c.cols + j] = m.data[i*m.cols + j];
+    int i, j;
+    matrix c = make_matrix(m.rows, m.cols * 2);
+    for (i = 0; i < m.rows; ++i) {
+        for (j = 0; j < m.cols; ++j) {
+            c.data[i * c.cols + j] = m.data[i * m.cols + j];
         }
     }
-    for(j = 0; j < m.rows; ++j){
-        c.data[j*c.cols + j+m.cols] = 1;
+    for (j = 0; j < m.rows; ++j) {
+        c.data[j * c.cols + j + m.cols] = 1;
     }
     return c;
 }
@@ -151,66 +168,65 @@ matrix matrix_invert(matrix m)
 {
     int i, j, k;
     //print_matrix(m);
-    matrix none = {0};
-    if(m.rows != m.cols){
+    matrix none = { 0 };
+    if (m.rows != m.cols) {
         fprintf(stderr, "Matrix not square\n");
         return none;
     }
     matrix c = augment_matrix(m);
     //print_matrix(c);
-    float **cdata = calloc(c.rows, sizeof(float *));
-    for(i = 0; i < c.rows; ++i){
-        cdata[i] = c.data + i*c.cols;
+    float** cdata = calloc(c.rows, sizeof(float*));
+    for (i = 0; i < c.rows; ++i) {
+        cdata[i] = c.data + i * c.cols;
     }
 
-
-    for(k = 0; k < c.rows; ++k){
+    for (k = 0; k < c.rows; ++k) {
         float p = 0.;
         int index = -1;
-        for(i = k; i < c.rows; ++i){
+        for (i = k; i < c.rows; ++i) {
             float val = fabs(cdata[i][k]);
-            if(val > p){
+            if (val > p) {
                 p = val;
                 index = i;
             }
         }
-        if(index == -1){
+        if (index == -1) {
             fprintf(stderr, "Can't do it, sorry!\n");
             free_matrix(c);
             return none;
         }
 
-        float *swap = cdata[index];
+        float* swap = cdata[index];
         cdata[index] = cdata[k];
         cdata[k] = swap;
 
         float val = cdata[k][k];
         cdata[k][k] = 1;
-        for(j = k+1; j < c.cols; ++j){
+        for (j = k + 1; j < c.cols; ++j) {
             cdata[k][j] /= val;
         }
-        for(i = k+1; i < c.rows; ++i){
+        for (i = k + 1; i < c.rows; ++i) {
             float s = -cdata[i][k];
             cdata[i][k] = 0;
-            for(j = k+1; j < c.cols; ++j){
-                cdata[i][j] +=  s*cdata[k][j];
+            for (j = k + 1; j < c.cols; ++j) {
+                cdata[i][j] += s * cdata[k][j];
             }
         }
     }
-    for(k = c.rows-1; k > 0; --k){
-        for(i = 0; i < k; ++i){
+    for (k = c.rows - 1; k > 0; --k) {
+        for (i = 0; i < k; ++i) {
             float s = -cdata[i][k];
             cdata[i][k] = 0;
-            for(j = k+1; j < c.cols; ++j){
-                cdata[i][j] += s*cdata[k][j];
+            for (j = k + 1; j < c.cols; ++j) {
+                cdata[i][j] += s * cdata[k][j];
             }
         }
     }
     //print_matrix(c);
     matrix inv = make_matrix(m.rows, m.cols);
-    for(i = 0; i < m.rows; ++i){
-        for(j = 0; j < m.cols; ++j){
-            inv.data[i*m.cols + j] = cdata[i][j+m.cols];
+    for (i = 0; i < m.rows; ++i) {
+        for (j = 0; j < m.cols; ++j) {
+            inv.data[i * m.cols + j] = cdata[i][j + m.cols];
         }
     }
     free_matrix(c);
@@ -221,41 +237,45 @@ matrix matrix_invert(matrix m)
 
 matrix solve_system(matrix M, matrix b)
 {
-    matrix none = {0};
+    matrix none = { 0 };
     matrix Mt = transpose_matrix(M);
     matrix MtM = matmul(Mt, M);
     matrix MtMinv = matrix_invert(MtM);
-    if(!MtMinv.data) return none;
+    if (!MtMinv.data)
+        return none;
     matrix Mdag = matmul(MtMinv, Mt);
     matrix a = matmul(Mdag, b);
-    free_matrix(Mt); free_matrix(MtM); free_matrix(MtMinv); free_matrix(Mdag);
+    free_matrix(Mt);
+    free_matrix(MtM);
+    free_matrix(MtMinv);
+    free_matrix(Mdag);
     return a;
 }
 
-void write_matrix(matrix m, FILE *fp)
+void write_matrix(matrix m, FILE* fp)
 {
-    fwrite(m.data, sizeof(float), m.rows*m.cols, fp);
+    fwrite(m.data, sizeof(float), m.rows * m.cols, fp);
 }
 
-void read_matrix(matrix m, FILE *fp)
+void read_matrix(matrix m, FILE* fp)
 {
-     fread(m.data, sizeof(float), m.rows*m.cols, fp);
+    fread(m.data, sizeof(float), m.rows * m.cols, fp);
 }
 
-void save_matrix(matrix m, char *fname)
+void save_matrix(matrix m, char* fname)
 {
-    FILE *fp = fopen(fname, "wb");
+    FILE* fp = fopen(fname, "wb");
     fwrite(&m.rows, sizeof(int), 1, fp);
     fwrite(&m.cols, sizeof(int), 1, fp);
     write_matrix(m, fp);
     fclose(fp);
 }
 
-matrix load_matrix(char *fname)
+matrix load_matrix(char* fname)
 {
     int rows = 0;
     int cols = 0;
-    FILE *fp = fopen(fname, "rb");
+    FILE* fp = fopen(fname, "rb");
     fread(&rows, sizeof(int), 1, fp);
     fread(&cols, sizeof(int), 1, fp);
     matrix m = make_matrix(rows, cols);
@@ -266,11 +286,11 @@ matrix load_matrix(char *fname)
 void test_matrix()
 {
     int i;
-    for(i = 0; i < 100; ++i){
-        int s = rand()%4 + 3;
+    for (i = 0; i < 100; ++i) {
+        int s = rand() % 4 + 3;
         matrix m = random_matrix(s, s, 10);
         matrix inv = matrix_invert(m);
-        if(inv.data){
+        if (inv.data) {
             matrix res = matmul(m, inv);
             print_matrix(res);
         }
